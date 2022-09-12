a54eced5c5563e0728ccdbec57f5f9cb97c8c18f
Marta Rubio <93192647+Martarubio93@users.noreply.github.com>
2022-09-12 17:36:23 +0100

    removed folder

--- my-calculator/src/components/Keypad.js
+++ my-calculator/src/components/Keypad.js
@@ -1,6 +1,6 @@
 import "../styles/layout/Keypad.scss";
 import OperatorsContext from "./OperatorsContext";
-import ResetBtn from './ResetBtn';
+import ResetBtn from "./ResetBtn";
 import React from "react";
 
 const Keypad = () => {
@@ -14,40 +14,64 @@ const Keypad = () => {
     setNumber1,
     setNumber2,
     setTotal,
-    displayTotal,
     setDisplayTotal,
-
   } = React.useContext(OperatorsContext);
 
+  const add = () => {
+    let newNumber1 = 0;
+    let newNumber2 = 0;
+    for (let i = 0; i < number1.length; i++) {
+      let currentNumber1 = number1[i];
+      newNumber1 = newNumber1 * 10 + currentNumber1;
+    }
+    for (let i = 0; i < number2.length; i++) {
+      let currentNumber2 = number2[i];
+      newNumber2 = newNumber2 * 10 + currentNumber2;
+    }
+    return setTotal(newNumber1 + newNumber2);
+  };
 
-const add = () => {
-  let newNumber1 = 0;
-  let newNumber2= 0;
-for (let i=0; i<number1.length; i++){
-  let currentNumber1 = number1[i];
-  newNumber1 = (newNumber1*10)+currentNumber1;
-}
-for (let i=0; i<number2.length; i++){
-  let currentNumber2 = number2[i];
-  newNumber2 = (newNumber2*10)+currentNumber2;
-}
- return setTotal(newNumber1+newNumber2)
-}
+  const substr = () => {
+    let newNumber1 = 0;
+    let newNumber2 = 0;
+    for (let i = 0; i < number1.length; i++) {
+      let currentNumber1 = number1[i];
+      newNumber1 = newNumber1 * 10 + currentNumber1;
+    }
+    for (let i = 0; i < number2.length; i++) {
+      let currentNumber2 = number2[i];
+      newNumber2 = newNumber2 * 10 + currentNumber2;
+    }
+    return setTotal(newNumber1 - newNumber2);
+  };
 
-const substr = () => {
-  let newNumber1 = 0;
-  let newNumber2= 0;
-  for (let i=0; i<number1.length; i++){
-    let currentNumber1 = number1[i];
-    newNumber1 = (newNumber1*10)+currentNumber1;
-  }
-  for (let i=0; i<number2.length; i++){
-    let currentNumber2 = number2[i];
-    newNumber2 = (newNumber2*10)+currentNumber2;
-  }
-   return setTotal(newNumber1-newNumber2)
-}
+  const mult = () => {
+    let newNumber1 = 0;
+    let newNumber2 = 0;
+    for (let i = 0; i < number1.length; i++) {
+      let currentNumber1 = number1[i];
+      newNumber1 = newNumber1 * 10 + currentNumber1;
+    }
+    for (let i = 0; i < number2.length; i++) {
+      let currentNumber2 = number2[i];
+      newNumber2 = newNumber2 * 10 + currentNumber2;
+    }
+    return setTotal(newNumber1 * newNumber2);
+  };
 
+  const div = () => {
+    let newNumber1 = 0;
+    let newNumber2 = 0;
+    for (let i = 0; i < number1.length; i++) {
+      let currentNumber1 = number1[i];
+      newNumber1 = newNumber1 * 10 + currentNumber1;
+    }
+    for (let i = 0; i < number2.length; i++) {
+      let currentNumber2 = number2[i];
+      newNumber2 = newNumber2 * 10 + currentNumber2;
+    }
+    return setTotal(newNumber1 / newNumber2);
+  };
 
   const handleOperator = (ev) => {
     handleDisplayOperator(ev.currentTarget.value);
@@ -55,34 +79,38 @@ const substr = () => {
   };
 
   const checkOperator = () => {
-    if (displayOperator.includes("+")){
-      add()
-    }else if (displayOperator.includes("-")){
-      substr()
-    }else if (displayOperator.includes("=")){
-      setDisplayTotal(true)
+    if (displayOperator.includes("+")) {
+      add();
+    } else if (displayOperator.includes("-")) {
+      substr();
+    } else if (displayOperator.includes("*")) {
+      mult();
+    } else if (displayOperator.includes("/")) {
+        div(); 
+    } else if (displayOperator.includes("=")) {
+      setDisplayTotal(true);
     }
-  }
+  };
 
   const checkIf = (e) => {
     if (displayOperator.length < 1) {
-      setNumber1([...number1, +e.currentTarget.value])
-  
-    }else setNumber2([...number2,+e.currentTarget.value ])
-  }
-
+      setNumber1([...number1, +e.currentTarget.value]);
+    } else setNumber2([...number2, +e.currentTarget.value]);
+  };
 
   return (
     <div className="container">
       {numbers.map((num, index) => {
-        return  <input
-        className="container__btn"
-        type="text"
-        key={index}
-        value={num}
-        onClick={checkIf}
-        readOnly
-       />;
+        return (
+          <input
+            className="container__btn"
+            type="text"
+            key={index}
+            value={num}
+            onClick={checkIf}
+            readOnly
+          />
+        );
       })}
       {operators.map((oper) => {
         return (
@@ -95,9 +123,8 @@ const substr = () => {
           />
         );
       })}
-            <ResetBtn/>
+      <ResetBtn />
     </div>
-    
   );
 };
 
--- my-calculator/src/components/Screen.js
+++ my-calculator/src/components/Screen.js
@@ -5,22 +5,31 @@ import OperatorsContext from "./OperatorsContext";
 import { useContext } from "react";
 
 const Screen = () => {
-  const {
-    displayOperator,
-    number1,
-    number2,
-    total,
-  } = useContext(OperatorsContext);
+  const { displayOperator, number1, number2, total } =
+    useContext(OperatorsContext);
 
   return (
     <div className="screen">
       <div className="screen__numbers">
-        <input className="screen__num" type="text" value={number1.join("") }  readOnly/>
-        <input className="screen__num" value={displayOperator.join("")}  readOnly/>
-        <input className="screen__num" type="text" value={number2.join("")}  readOnly />
-
+        <input
+          className="screen__num"
+          type="text"
+          value={number1.join("")}
+          readOnly
+        />
+        <input
+          className="screen__num"
+          value={displayOperator.join("")}
+          readOnly
+        />
+        <input
+          className="screen__num"
+          type="text"
+          value={number2.join("")}
+          readOnly
+        />
       </div>
-      <h2 className="screen__result" > {total} </h2>
+      <h2 className="screen__result"> {total} </h2>
     </div>
   );
 };
